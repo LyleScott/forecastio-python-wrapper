a430ef4927e7cc4520c22ff6f54479f7440c30ea
Lyle <lyscott@qualitydistribution.com>
2014-10-05 19:55:03 -0400

    add new example

--- README.md
+++ README.md
@@ -21,41 +21,15 @@ Example
 -------
 
 ```python
-def _pretty_json(data):
-    print json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
 
+from forecastiowrap import ForecastioWrapper
 
-def get_request_json():
-    """Get the raw JSON response from an API call."""
-    json_response = get_json(37.8267, -122.423)
-    print _pretty_json(json_response)
+API_KEY = 'fba3b9ccabb3c66e29a4f18e7502d126'
 
+forecastio = ForecastioWrapper(API_KEY)
+# 27.7731 N, 82.6400 W   (Saint Petersburg, Florida, USA)
+location = forecastio.get_location(27.7731, 82.6400)
 
-def get_request_json_with_time():
-    """Get the raw JSON response from an API call for a specific time."""
-    time_ = time()
-    json_response = get_json(37.8267, -122.423, time_)
-    print _pretty_json(json_response)
-
-
-def get_location_object():
-    """Serialize an API request to a Location object representing all elements
-    of the API response (see models/location.py docstrings for more info).
-    """
-    location = get_location(37.8267, -122.423)
-    print 'Location', location
-    print 'Alerts', location.alerts
-    if location.alerts:
-        print 'Alert', location.alerts[0].description
-    print 'Currently', location.currently.temperature
-
-
-def get_multiple_locations():
-    """Serialize multiple coordinates into an iterable of Location objects."""
-    coords = ((34.2233, 77.9122), (27.7731, 82.6400))
-    locations = get_locations(coords)
-    for location in locations:
-        print location.currently.temperature
 ```
 
 
--- example/example.py
+++ example/example.py
@@ -1,55 +1,30 @@
-# This is just a fix to put the module into PYTHONPATH
-import os, sys
-sys.path.insert(1, os.path.join(os.path.dirname(os.path.dirname(__file__)), 'src'))
+from pprint import pprint
+from forecastiowrap import ForecastioWrapper
 
-import json
-from time import time
+API_KEY = 'fba3b9ccabb3c66e29a4f18e7502d126'
 
-from api import get_json
-from api import get_location
-from api import get_locations
+forecastio = ForecastioWrapper(API_KEY)
 
+# 27.7731 N, 82.6400 W  ==  Saint Petersburg, Florida, USA
+location = forecastio.get_location(27.7731, 82.6400)
+print(location)
+print(location.hourly[0].temperature)
+pprint([attr for attr in dir(location) if not attr.startswith('_')])
 
-def _pretty_json(data):
-    print json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
+print(location.currently)
+pprint([attr for attr in dir(location.currently) if not attr.startswith('_')])
 
+print(location.minutely)
+pprint([attr for attr in dir(location.minutely) if not attr.startswith('_')])
 
-def get_request_json():
-    """Get the raw JSON response from an API call."""
-    json_response = get_json(37.8267, -122.423)
-    print _pretty_json(json_response)
+print(location.hourly)
+pprint([attr for attr in dir(location.hourly) if not attr.startswith('_')])
 
+print(location.daily)
+pprint([attr for attr in dir(location.daily) if not attr.startswith('_')])
 
-def get_request_json_with_time():
-    """Get the raw JSON response from an API call for a specific time."""
-    time_ = time()
-    json_response = get_json(37.8267, -122.423, time_)
-    print _pretty_json(json_response)
+print(location.alerts)
+pprint([attr for attr in dir(location.alerts) if not attr.startswith('_')])
 
-
-def get_location_object():
-    """Serialize an API request to a Location object representing all elements
-    of the API response (see models/location.py docstrings for more info).
-    """
-    location = get_location(37.8267, -122.423)
-    print 'Location', location
-    print 'Alerts', location.alerts
-    if location.alerts:
-        print 'Alert', location.alerts[0].description
-    print 'Currently', location.currently.temperature
-
-
-def get_multiple_locations():
-    """Serialize multiple coordinated into an iterable of Location objects."""
-    coords = ((34.2233, 77.9122), (27.7731, 82.6400))
-    locations = get_locations(coords)
-    for location in locations:
-        print location.currently.temperature
-
-
-if __name__ == '__main__':
-    """Run different examples."""
-    get_request_json()
-    #get_request_json_with_time()
-    #get_location_object()
-    #get_multiple_locations()
+print(location.flags)
+pprint([attr for attr in dir(location.flags) if not attr.startswith('_')])
